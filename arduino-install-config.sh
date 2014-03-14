b5d6563138464f8a35df6b9d92d052b3edda12e3
Sean Baskin <seanbaskin@gmail.com>
2014-03-14 11:03:24 -0400

    Added friendly message to logout

--- arduino-install-config.sh
+++ arduino-install-config.sh
@@ -5,3 +5,6 @@ sudo apt-get update && sudo apt-get install -y arduino arduino-core
 
 # Add user to appropriate groups
 sudo usermod -aG dialout,serial `whoami`
+
+# Notify user to logout
+echo 'Please logout of your current *nix session for group changes to take effect."
